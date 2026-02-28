ec2f53dd7cc72fca7899127eb7dd7ff74fc6109a
DraScript <203324047+INDRA969696@users.noreply.github.com>
2026-02-28 09:29:20 +0700

    Update Main.lua

--- UnfairFlips/Main.lua
+++ UnfairFlips/Main.lua
@@ -1,9 +1,9 @@
 local DraLibs = loadstring(game:HttpGet("https://raw.githubusercontent.com/INDRA969696/DrScriptHub/refs/heads/main/DraLibrary/DraLibs"))()
-local hub = DraLibs:CreateWindow("Unfair Flip")
-hub:CreateButton("Inf Coin", function()
+local hub = DraLibs:window("Unfair Flip")
+hub:button("Inf Coin", function()
   game:GetService("ReplicatedStorage").GiveMoney:FireServer(9e500)
 end)
 
-hub:CreateButton("Inf Gem", function() game:GetService("ReplicatedStorage").GemUpgrades:FireServer("ExtraCardOptions", -9e500 , 1, 2) 
+hub:button("Inf Gem", function() game:GetService("ReplicatedStorage").GemUpgrades:FireServer("ExtraCardOptions", -9e500 , 1, 2) 
 end
 )
